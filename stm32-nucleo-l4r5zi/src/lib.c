da612271bc445aee271b30ae94a3b98b44f246eb
thomasw04 <35061939+thomasw04@users.noreply.github.com>
2024-11-30 12:42:40 +0100

    stm32-nucleo-l4r5zi: make handlers weak.

--- stm32-nucleo-l4r5zi/src/lib.c
+++ stm32-nucleo-l4r5zi/src/lib.c
@@ -1,629 +1,629 @@
 #include <hal/lib.h>
 
-void wwdg_hndlr(void)
+void __attribute__((weak)) wwdg_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void pvd_pvm_hndlr(void)
+void __attribute__((weak)) pvd_pvm_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void tamp_stamp_hndlr(void)
+void __attribute__((weak)) tamp_stamp_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void rtc_wkup_hndlr(void)
+void __attribute__((weak)) rtc_wkup_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void flash_hndlr(void)
+void __attribute__((weak)) flash_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void rcc_hndlr(void)
+void __attribute__((weak)) rcc_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void exti0_hndlr(void)
+void __attribute__((weak)) exti0_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void exti1_hndlr(void)
+void __attribute__((weak)) exti1_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void exti2_hndlr(void)
+void __attribute__((weak)) exti2_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void exti3_hndlr(void)
+void __attribute__((weak)) exti3_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void exti4_hndlr(void)
+void __attribute__((weak)) exti4_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void dma1_ch1_hndlr(void)
+void __attribute__((weak)) dma1_ch1_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void dma1_ch2_hndlr(void)
+void __attribute__((weak)) dma1_ch2_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void dma1_ch3_hndlr(void)
+void __attribute__((weak)) dma1_ch3_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void dma1_ch4_hndlr(void)
+void __attribute__((weak)) dma1_ch4_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void dma1_ch5_hndlr(void)
+void __attribute__((weak)) dma1_ch5_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void dma1_ch6_hndlr(void)
+void __attribute__((weak)) dma1_ch6_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void dma1_ch7_hndlr(void)
+void __attribute__((weak)) dma1_ch7_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void adc1_hndlr(void)
+void __attribute__((weak)) adc1_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void can1_tx_hndlr(void)
+void __attribute__((weak)) can1_tx_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void can1_rx0_hndlr(void)
+void __attribute__((weak)) can1_rx0_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void can1_rx1_hndlr(void)
+void __attribute__((weak)) can1_rx1_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void can1_sce_hndlr(void)
+void __attribute__((weak)) can1_sce_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void exti9_5_hndlr(void)
+void __attribute__((weak)) exti9_5_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void tim1_brk_tim15_hndlr(void)
+void __attribute__((weak)) tim1_brk_tim15_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void tim1_up_tim16_hndlr(void)
+void __attribute__((weak)) tim1_up_tim16_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void tim1_trg_com_tim17_hndlr(void)
+void __attribute__((weak)) tim1_trg_com_tim17_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void tim1_cc_hndlr(void)
+void __attribute__((weak)) tim1_cc_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void tim2_hndlr(void)
+void __attribute__((weak)) tim2_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void tim3_hndlr(void)
+void __attribute__((weak)) tim3_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void tim4_hndlr(void)
+void __attribute__((weak)) tim4_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void i2c1_ev_hndlr(void)
+void __attribute__((weak)) i2c1_ev_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void i2c1_er_hndlr(void)
+void __attribute__((weak)) i2c1_er_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void i2c2_ev_hndlr(void)
+void __attribute__((weak)) i2c2_ev_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void i2c2_er_hndlr(void)
+void __attribute__((weak)) i2c2_er_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void spi1_hndlr(void)
+void __attribute__((weak)) spi1_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void spi2_hndlr(void)
+void __attribute__((weak)) spi2_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void usart1_hndlr(void)
+void __attribute__((weak)) usart1_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void usart2_hndlr(void)
+void __attribute__((weak)) usart2_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void usart3_hndlr(void)
+void __attribute__((weak)) usart3_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void exti15_10_hndlr(void)
+void __attribute__((weak)) exti15_10_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void rtc_alarm_hndlr(void)
+void __attribute__((weak)) rtc_alarm_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void dfsdm1_flt3_hndlr(void)
+void __attribute__((weak)) dfsdm1_flt3_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void tim8_brk_hndlr(void)
+void __attribute__((weak)) tim8_brk_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void tim8_up_hndlr(void)
+void __attribute__((weak)) tim8_up_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void tim8_trg_com_hndlr(void)
+void __attribute__((weak)) tim8_trg_com_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void tim8_cc_hndlr(void)
+void __attribute__((weak)) tim8_cc_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void fmc_hndlr(void)
+void __attribute__((weak)) fmc_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void sdmmc1_hndlr(void)
+void __attribute__((weak)) sdmmc1_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void tim5_hndlr(void)
+void __attribute__((weak)) tim5_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void spi3_hndlr(void)
+void __attribute__((weak)) spi3_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void uart4_hndlr(void)
+void __attribute__((weak)) uart4_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void uart5_hndlr(void)
+void __attribute__((weak)) uart5_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void tim6_dac_under_hndlr(void)
+void __attribute__((weak)) tim6_dac_under_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void tim7_hndlr(void)
+void __attribute__((weak)) tim7_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void dma2_ch1_hndlr(void)
+void __attribute__((weak)) dma2_ch1_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void dma2_ch2_hndlr(void)
+void __attribute__((weak)) dma2_ch2_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void dma2_ch3_hndlr(void)
+void __attribute__((weak)) dma2_ch3_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void dma2_ch4_hndlr(void)
+void __attribute__((weak)) dma2_ch4_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void dma2_ch5_hndlr(void)
+void __attribute__((weak)) dma2_ch5_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void dfsdm1_flt0_hndlr(void)
+void __attribute__((weak)) dfsdm1_flt0_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void dfsdm1_flt1_hndlr(void)
+void __attribute__((weak)) dfsdm1_flt1_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void dfsdm1_flt2_hndlr(void)
+void __attribute__((weak)) dfsdm1_flt2_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void comp_hndlr(void)
+void __attribute__((weak)) comp_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void lptim1_hndlr(void)
+void __attribute__((weak)) lptim1_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void lptim2_hndlr(void)
+void __attribute__((weak)) lptim2_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void otg_fs_hndlr(void)
+void __attribute__((weak)) otg_fs_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void dma2_ch6_hndlr(void)
+void __attribute__((weak)) dma2_ch6_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void dma2_ch7_hndlr(void)
+void __attribute__((weak)) dma2_ch7_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void lpuart1_hndlr(void)
+void __attribute__((weak)) lpuart1_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void octospi1_hndlr(void)
+void __attribute__((weak)) octospi1_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void i2c3_ev_hndlr(void)
+void __attribute__((weak)) i2c3_ev_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void i2c3_er_hndlr(void)
+void __attribute__((weak)) i2c3_er_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void sai1_hndlr(void)
+void __attribute__((weak)) sai1_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void sai2_hndlr(void)
+void __attribute__((weak)) sai2_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void octospi2_hndlr(void)
+void __attribute__((weak)) octospi2_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void tsc_hndlr(void)
+void __attribute__((weak)) tsc_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void dsihot_hndlr(void)
+void __attribute__((weak)) dsihot_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void aes_hndlr(void)
+void __attribute__((weak)) aes_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void rng_hndlr(void)
+void __attribute__((weak)) rng_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void fpu_hndlr(void)
+void __attribute__((weak)) fpu_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void hash_crs_hndlr(void)
+void __attribute__((weak)) hash_crs_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void i2c4_er_hndlr(void)
+void __attribute__((weak)) i2c4_er_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void i2c4_ev_hndlr(void)
+void __attribute__((weak)) i2c4_ev_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void dcmi_hndlr(void)
+void __attribute__((weak)) dcmi_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void dma2d_hndlr(void)
+void __attribute__((weak)) dma2d_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void lcd_tft_hndlr(void)
+void __attribute__((weak)) lcd_tft_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void lcd_tft_er_hndlr(void)
+void __attribute__((weak)) lcd_tft_er_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void gfxmmu_hndlr(void)
+void __attribute__((weak)) gfxmmu_hndlr(void)
 {
     while (1)
     {
         __asm__("wfi");
     }
 }
-void dmamux1_ovr_hndlr(void)
+void __attribute__((weak)) dmamux1_ovr_hndlr(void)
 {
     while (1)
     {
