cc6c571a0e601f8548f0038fecd49a102282a169
Jakob Fuchs <ja.fuchs@tum.de>
2024-12-22 13:39:07 +0100

    GPIO rewrite, take/release+state transition+outputtype

--- stm32-nucleo-l4r5zi/src/gpio.rs
+++ stm32-nucleo-l4r5zi/src/gpio.rs
@@ -1,3 +1,5 @@
+///preliminary version, untested code
+
 use core::sync::atomic::{AtomicU32, AtomicU8, Ordering};
 use core::marker::{Copy, PhantomData};
 use core::ops::{Add};
@@ -70,9 +72,45 @@ pub enum Pin {
     PIN15 = 15,
 }
 
+#[derive(Copy, Clone)]
 pub enum OutputType {
     PushPull = 0,
     OpenDrain = 1,
+}
+
+#[derive(Copy, Clone)]
+pub enum Speed {
+    Low = 0,
+    Medium = 1,
+    High = 2,
+    VeryHigh = 3,
+}
+
+#[derive(Copy, Clone)]
+pub enum PushPullMode {
+    Disabled = 0,
+    PullUp = 1,
+    PullDown = 2,
+}
+
+#[derive(Copy, Clone)]
+pub enum AlternateFunction {
+    AF0 = 0,
+    AF1 = 1,
+    AF2 = 2,
+    AF3 = 3,
+    AF4 = 4,
+    AF5 = 5,
+    AF6 = 6,
+    AF7 = 7,
+    AF8 = 8,
+    AF9 = 9,
+    AF10 = 10,
+    AF11 = 11,
+    AF12 = 12,
+    AF13 = 13,
+    AF14 = 14,
+    AF15 = 15,
 }
 
 /// Available GPIO Modes
@@ -95,6 +133,7 @@ trait InputConvertible {}
 trait OutputConvertible {}
 trait AlternateConvertible {}
 trait AnalogConvertible {}
+trait Configured {}
 
 pub trait IntoInput<Success,Failure> {
     fn into_input(self) -> Result<Success,Failure>;
@@ -132,40 +171,46 @@ impl AlternateConvertible for Input {}
 impl AlternateConvertible for Output {}
 impl AlternateConvertible for Analog {}
 impl AlternateConvertible for Undefined {}
+impl Configured for Input {}
+impl Configured for Output {}
+impl Configured for Alternate {}
+impl Configured for Analog {}
 
 
 
 impl <Mode> GPIOPin<Mode> {
-    fn get_baseaddress(&self) -> u32{
+    fn get_baseaddress(&self) -> u32 {
         match self.port {
-            Port::GPIOA => {GPIOA_BASE}
-            Port::GPIOB => {GPIOB_BASE}
-            Port::GPIOC => {GPIOA_BASE}
-            Port::GPIOD => {GPIOA_BASE}
-            Port::GPIOE => {GPIOA_BASE}
-            Port::GPIOF => {GPIOA_BASE}
-            Port::GPIOG => {GPIOA_BASE}
-            Port::GPIOH => {GPIOA_BASE}
-            Port::GPIOI => {GPIOA_BASE}
+            Port::GPIOA => { GPIOA_BASE }
+            Port::GPIOB => { GPIOB_BASE }
+            Port::GPIOC => { GPIOA_BASE }
+            Port::GPIOD => { GPIOA_BASE }
+            Port::GPIOE => { GPIOA_BASE }
+            Port::GPIOF => { GPIOA_BASE }
+            Port::GPIOG => { GPIOA_BASE }
+            Port::GPIOH => { GPIOA_BASE }
+            Port::GPIOI => { GPIOA_BASE }
         }
     }
 
     pub fn release(self) {
-        let idx = 2*self.port as usize;
+        let idx = 2 * self.port as usize;
         let idx = if (self.pin as usize) < 8 {
             idx
         } else {
-            idx+1
+            idx + 1
         };
         let bit = if (self.pin as usize) < 8 {
             self.pin as usize
         } else {
-            self.pin as usize-8
+            self.pin as usize - 8
         };
-        let mask = !(1<<bit);
+        let mask = !(1 << bit);
         TAKEN[idx].fetch_and(mask, Ordering::AcqRel);
     }
+}
 
+impl <Mode: Configured> GPIOPin<Mode> {
     pub fn set_outputtype(&mut self,r#type:OutputType) {
         let target = 1<<self.pin as usize;
         let baseaddress = self.get_baseaddress().add(OTYPER_OFFSET);
@@ -180,6 +225,120 @@ impl <Mode> GPIOPin<Mode> {
             OutputType::OpenDrain => {
                 atomic.fetch_or(target, Ordering::AcqRel);
             }
+        }
+    }
+
+    pub fn set_speed(&mut self,speed:Speed) {
+        let target = 11<<2*self.pin as usize;
+        let speedpattern = match speed {
+            Speed::Low => {0b0000_0000_0000_0000_0000_0000_0000_0000}
+            Speed::Medium => {0b0101_0101_0101_0101_0101_0101_0101_0101}
+            Speed::High => {0b1010_1010_1010_1010_1010_1010_1010_1010_}
+            Speed::VeryHigh => {0b1111_1111_1111_1111_1111_1111_1111_1111_}
+        };
+        let baseaddress = self.get_baseaddress().add(OSPEEDR_OFFSET);
+        let ptr: *mut u32 = baseaddress as *mut u32;
+        ///SAFETY:
+        /// The Base Address is on of 9 possible base addresses that are memory mapped registers and therefor guaranteed to be valid
+        let atomic: &AtomicU32 = unsafe { AtomicU32::from_ptr(ptr) };
+        ///SAFETY
+        /// This Result is only error if the closure returns None, which can't happen
+        atomic.fetch_update(Ordering::AcqRel, Ordering::Relaxed, |value| {
+            let cleared = value & !target;
+            Some(cleared | (target&speedpattern))
+        }).expect("UNREACHABLE");
+    }
+
+    pub fn set_push_pull(&mut self,pupd:PushPullMode) {
+        let target = 11<<2*self.pin as usize;
+        let speedpattern = match pupd {
+            PushPullMode::Disabled => {0b0000_0000_0000_0000_0000_0000_0000_0000}
+            PushPullMode::PullUp => {0b0101_0101_0101_0101_0101_0101_0101_0101}
+            PushPullMode::PullDown => {0b1010_1010_1010_1010_1010_1010_1010_1010_}
+        };
+        let baseaddress = self.get_baseaddress().add(PUPDR_OFFSET);
+        let ptr: *mut u32 = baseaddress as *mut u32;
+        ///SAFETY:
+        /// The Base Address is on of 9 possible base addresses that are memory mapped registers and therefor guaranteed to be valid
+        let atomic: &AtomicU32 = unsafe { AtomicU32::from_ptr(ptr) };
+        ///SAFETY
+        /// This Result is only error if the closure returns None, which can't happen
+        atomic.fetch_update(Ordering::AcqRel, Ordering::Relaxed, |value| {
+            let cleared = value & !target;
+            Some(cleared | (target&speedpattern))
+        }).expect("UNREACHABLE");
+    }
+
+    pub fn set_alternate_function(&mut self,af:AlternateFunction) {
+        let target = if (self.pin as usize) < 8 {
+            1111 << 4 * self.pin as usize
+        } else {
+            1111 << 4 * (self.pin as usize - 8)
+        };
+        let speedpattern = match af {
+            AlternateFunction::AF0 => { 0b0000_0000_0000_0000_0000_0000_0000_0000 }
+            AlternateFunction::AF1 => { 0b0001_0001_0001_0001_0001_0001_0001_0001 }
+            AlternateFunction::AF2 => { 0b0010_0010_0010_0010_0010_0010_0010_0010 }
+            AlternateFunction::AF3 => { 0b0011_0011_0011_0011_0011_0011_0011_0011 }
+            AlternateFunction::AF4 => { 0b0100_0100_0100_0100_0100_0100_0100_0100 }
+            AlternateFunction::AF5 => { 0b0101_0101_0101_0101_0101_0101_0101_0101 }
+            AlternateFunction::AF6 => { 0b0110_0110_0110_0110_0110_0110_0110_0110 }
+            AlternateFunction::AF7 => { 0b0111_0111_0111_0111_0111_0111_0111_0111 }
+            AlternateFunction::AF8 => { 0b1000_1000_1000_1000_1000_1000_1000_1000 }
+            AlternateFunction::AF9 => { 0b1001_1001_1001_1001_1001_1001_1001_1001 }
+            AlternateFunction::AF10 => { 0b1010_1010_1010_1010_1010_1010_1010_1010 }
+            AlternateFunction::AF11 => { 0b1011_1011_1011_1011_1011_1011_1011_1011 }
+            AlternateFunction::AF12 => { 0b0000_0000_0000_0000_0000_0000_0000_0000 }
+            AlternateFunction::AF13 => { 0b0000_0000_0000_0000_0000_0000_0000_0000 }
+            AlternateFunction::AF14 => { 0b0000_0000_0000_0000_0000_0000_0000_0000 }
+            AlternateFunction::AF15 => { 0b0000_0000_0000_0000_0000_0000_0000_0000 }
+        };
+        let baseaddress: u32 = if (self.pin as usize) < 8 {
+            self.get_baseaddress().add(AFRL_OFFSET)
+        } else {
+            self.get_baseaddress().add(AFRL_OFFSET)
+        };
+        let ptr: *mut u32 = baseaddress as *mut u32;
+        ///SAFETY:
+        /// The Base Address is on of 9 possible base addresses that are memory mapped registers and therefor guaranteed to be valid
+        let atomic: &AtomicU32 = unsafe { AtomicU32::from_ptr(ptr) };
+        ///SAFETY
+        /// This Result is only error if the closure returns None, which can't happen
+        atomic.fetch_update(Ordering::AcqRel, Ordering::Relaxed, |value| {
+            let cleared = value & !target;
+            Some(cleared | (target & speedpattern))
+        }).expect("UNREACHABLE");
+    }
+
+    pub fn set_pin(&mut self) {
+        let baseaddress = self.get_baseaddress().add(BSRR_OFFSET);
+        let ptr: *mut u32 = baseaddress as *mut u32;
+        let target = 1 << self.pin as usize;
+        ///SAFETY:
+        /// The Base Address is on of 9 possible base addresses that are memory mapped registers and therefor guaranteed to be valid
+        unsafe { ptr.write(target); }
+    }
+
+    pub fn reset_pin(&mut self) {
+        let baseaddress = self.get_baseaddress().add(BSRR_OFFSET);
+        let ptr: *mut u32 = baseaddress as *mut u32;
+        let target = 1 << (self.pin as usize +16);
+        ///SAFETY:
+        /// The Base Address is on of 9 possible base addresses that are memory mapped registers and therefor guaranteed to be valid
+        unsafe { ptr.write_volatile(target); }
+    }
+
+    pub fn read_state(&self) -> bool {
+        let baseaddress = self.get_baseaddress().add(IDR_OFFSET);
+        let ptr: *mut u32 = baseaddress as *mut u32;
+        let target = 1 << (self.pin as usize +16);
+        ///SAFETY:
+        /// The Base Address is on of 9 possible base addresses that are memory mapped registers and therefor guaranteed to be valid
+        let idr:u32 = unsafe { ptr.read_volatile() };
+        if (idr&target)==0 {
+            false
+        } else {
+            true
         }
     }
 }
