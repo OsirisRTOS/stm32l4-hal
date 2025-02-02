a1f576efbdbbc9c2986c3c0c0bdf74b43005703c
thomasw04 <35061939+thomasw04@users.noreply.github.com>
2025-02-02 22:55:36 +0100

    context switch now works.

--- stm32-nucleo-l4r5zi/src/lib.rs
+++ stm32-nucleo-l4r5zi/src/lib.rs
@@ -5,13 +5,37 @@ pub mod semih;
 
 pub extern crate cortex_m as common;
 
+use common::*;
+use core::ops::Add;
+
+const RCC_BASE:u32 = 0x40021000;
+const RCC_AHB2ENR_OFFSET:u32 =  0x4C;
+
 #[no_mangle]
 pub extern "C" fn default_hndlr() {
     loop {}
 }
 
 #[no_mangle]
-pub extern "C" fn hal_hw_init() {}
+pub extern "C" fn hal_hw_init() {
+    //enable GPIO peripheral clock
+    let ptr:*mut u32 = RCC_BASE.add(RCC_AHB2ENR_OFFSET) as *mut u32;
+    //SAFETY:
+    // This is a write to a known memory address which is a memory mapped register
+    unsafe { ptr.write_volatile(0x000001ff); }
+
+    init_systick();
+}
+
+fn init_systick() {
+    let mut systick = peripheral::SYST::new();
+    let reload = peripheral::SYST::get_ticks_per_10ms();
+
+    systick.enable_interrupt();
+    systick.set_reload(reload);
+    systick.clear_current();
+    systick.enable_counter();
+}
 
 /*
  * Function: hal_semih_write
