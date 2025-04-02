8244a6a5ff9f14b8ee288d350467b151089a8bf4
thomasw04 <35061939+thomasw04@users.noreply.github.com>
2025-04-02 19:37:29 +0000

    remove: cbindgen

--- stm32-nucleo-l4r5zi/build.rs
+++ stm32-nucleo-l4r5zi/build.rs
@@ -1,23 +1,3 @@
 extern crate cbindgen;
 
-fn main() {
-    let crate_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
-
-    let mut config: cbindgen::Config = Default::default();
-
-    config.no_includes = true;
-    config.includes = vec!["stdint.h".to_string(), "stdbool.h".to_string(), "stdarg.h".to_string()];
-
-    let bindings = cbindgen::Builder::new()
-        .with_config(config)
-        .with_crate(crate_dir)
-        .with_language(cbindgen::Language::C)
-        .with_include_guard("HAL_H")
-        .generate();
-
-    match bindings {
-        Ok(bindings) => bindings.write_to_file("include/hal/lib.h"),
-        Err(cbindgen::Error::ParseSyntaxError { .. }) => return, // ignore in favor of cargo's syntax check
-        Err(err) => panic!("{:?}", err),
-    };
-}
+fn main() {}
